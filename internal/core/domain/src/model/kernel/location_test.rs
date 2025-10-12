5c6b125579b16b8465763a5c96864d4d83d40d27
Nikita Elfimov <nelfimov@mail.ru>
2025-10-12 21:38:47 +0300

    feat(domain): init location

---
+++ internal/core/domain/src/model/kernel/location_test.rs
@@ -0,0 +1,29 @@
+use super::location::Location;
+
+#[test]
+fn gets_distance() {
+    let a = Location::new(5, 5).unwrap();
+    let b = Location::new(1, 2).unwrap();
+
+    assert_eq!(a.get_distance(&b), 7);
+    assert_eq!(b.get_distance(&a), 7);
+}
+
+#[test]
+fn creates_random() {
+    let a = Location::new_random();
+
+    assert!(a.x() >= 1 && a.x() <= 10);
+    assert!(a.y() >= 1 && a.y() <= 10);
+}
+
+#[test]
+fn compares() {
+    let a = Location::new(1, 1);
+    let b = Location::new(2, 2);
+    let c = Location::new(1, 1);
+
+    assert_ne!(a, b);
+    assert_eq!(a, c);
+    assert_ne!(b, c);
+}
---
+++ internal/core/domain/src/model/kernel/mod.rs
@@ -0,0 +1,3 @@
+pub mod location;
+#[cfg(test)]
+pub mod location_test;
