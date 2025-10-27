bdb28c93c18a173441c6569c1d7624baa8ccff7f
Nikita Elfimov <nelfimov@mail.ru>
2025-10-27 20:56:44 +0300

    fix(domain): modify vector of couriers and return a mut ref to courier

--- internal/core/domain/src/model/services/order_dispatcher.rs
+++ internal/core/domain/src/model/services/order_dispatcher.rs
@@ -1,5 +1,3 @@
-use std::cmp::Reverse;
-
 use crate::errors::domain_model_errors::DomainModelError;
 use crate::model::courier::courier_aggregate::Courier;
 use crate::model::kernel::volume::Volume;
@@ -7,19 +5,19 @@ use crate::model::order::order_aggregate::Order;
 use crate::model::order::order_aggregate::OrderStatus;
 
 pub trait OrderDispatcher {
-    fn dispatch(
+    fn dispatch<'a>(
         order: &mut Order,
-        couriers: &mut Vec<Courier>,
-    ) -> Result<Courier, DomainModelError>;
+        couriers: &'a mut Vec<Courier>,
+    ) -> Result<&'a mut Courier, DomainModelError>;
 }
 
 pub struct OrderDispatcherService;
 
 impl OrderDispatcher for OrderDispatcherService {
-    fn dispatch(
+    fn dispatch<'a>(
         order: &mut Order,
-        couriers: &mut Vec<Courier>,
-    ) -> Result<Courier, DomainModelError> {
+        couriers: &'a mut Vec<Courier>,
+    ) -> Result<&'a mut Courier, DomainModelError> {
         if order.status() != &OrderStatus::Created {
             return Err(DomainModelError::UnmetRequirement(
                 "status it not 'created'".into(),
@@ -29,33 +27,20 @@ impl OrderDispatcher for OrderDispatcherService {
         let order_volume = Volume::new(order.volume())
             .map_err(|_| DomainModelError::UnmetRequirement("invalid order volume".into()))?;
 
-        let mut available = Vec::new();
-        let mut i = 0;
+        let (idx, _) = couriers
+            .iter_mut()
+            .enumerate()
+            .filter(|(_, c)| c.can_take_order(&order_volume).is_some())
+            .min_by_key(|(_, c)| c.get_traverse_length(order.location()))
+            .ok_or(DomainModelError::UnmetRequirement(
+                "no courier found".into(),
+            ))?;
 
-        while i < couriers.len() {
-            if couriers[i].can_take_order(&order_volume).is_some() {
-                let c = couriers.remove(i);
-                available.push(c);
-            } else {
-                i += 1;
-            }
-        }
+        let courier = &mut couriers[idx];
 
-        available.sort_by_key(|c| Reverse(c.get_traverse_length(order.location())));
+        courier.take_order(order.id(), order_volume)?;
+        order.assign(courier.id())?;
 
-        let mut courier = available.pop().ok_or(DomainModelError::UnmetRequirement(
-            "no courier found".into(),
-        ))?;
-
-        match courier.take_order(order.id(), order_volume) {
-            Ok(_) => {
-                match order.assign(courier.id()) {
-                    Ok(_) => {}
-                    Err(e) => return Err(e),
-                }
-                Ok(courier)
-            }
-            Err(e) => Err(e),
-        }
+        Ok(courier)
     }
 }
