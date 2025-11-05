6c8c35eded37b395509c34c347b0b900b8287b68
Nikita Elfimov <nelfimov@mail.ru>
2025-11-05 20:12:54 +0300

    feat(entrypoint): init

---
+++ entrypoint/src/config.rs
@@ -0,0 +1,29 @@
+use envy::Error;
+use envy::from_env;
+use serde::Deserialize;
+
+fn default_server_address() -> String {
+    String::from("0.0.0.0")
+}
+
+fn default_server_port() -> u8 {
+    3000
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub db_host: String,
+    pub db_port: u16,
+    pub db_user: String,
+    pub db_password: String,
+    #[serde(default = "default_server_address")]
+    pub server_address: String,
+    #[serde(default = "default_server_port")]
+    pub server_port: u8,
+}
+
+impl Config {
+    pub fn from_env() -> Result<Self, Error> {
+        from_env::<Config>()
+    }
+}
---
+++ entrypoint/src/main.rs
@@ -0,0 +1,34 @@
+mod config;
+
+use in_http::server::start_server;
+use in_http::state::AppState;
+use out_postgres::connection::PgConnectionOptions;
+use out_postgres::connection::establish_connection;
+
+use config::Config;
+use out_postgres::courier::courier_repository::CourierRepository;
+use out_postgres::order::order_repository::OrderRepository;
+use out_postgres::unit_of_work::UnitOfWork;
+
+#[tokio::main]
+async fn main() {
+    let config = Config::from_env().expect("missing env variables");
+    let pool = establish_connection(PgConnectionOptions::new(
+        config.db_host,
+        config.db_port,
+        config.db_user,
+        config.db_password,
+    ));
+
+    let courier_repo = CourierRepository::new(&mut pool.get().expect("connection unavailable"));
+    let order_repo = OrderRepository::new(&mut pool.get().expect("connection unavailable"));
+    let uow = UnitOfWork::new(&mut pool.get().expect("connection unavailable"));
+
+    let app_state = AppState::new(courier_repo, order_repo, uow);
+
+    start_server(
+        &format!("{}:{}", config.server_address, config.server_port),
+        app_state,
+    )
+    .await;
+}
